7a4ce2bcca20ff71b4d5861e6239e9dd32833fd0
GeekishGirl <tusek.nicole@gmail.com>
2017-02-27 20:43:39 -0800

    Finished character counter.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+/* Creating the character counter! */
 
+var charactersLeft = 240;
+
+$("#tweetArea").keyup(function(e){
+    var characters = $("#tweetArea").val().length;
+    charactersLeft = (240 - (characters));
+    $("#characterCount").replaceWith("<label id=" + 'characterCount' + ">Character(s) Left: " + charactersLeft + "</label>");
+});
